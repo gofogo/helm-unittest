29a34b0854d144361e7185b5f7ad2ad0caf80f2d
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-01-18 10:33:09 +0000

    fix(issue-534): work out fix for yaml separation
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/unittest/test_job.go
+++ pkg/unittest/test_job.go
@@ -1,7 +1,6 @@
 package unittest
 
 import (
-	"bytes"
 	"cmp"
 	"fmt"
 	"io"
@@ -21,8 +20,6 @@ import (
 	v3chart "helm.sh/helm/v3/pkg/chart"
 	v3util "helm.sh/helm/v3/pkg/chartutil"
 	v3engine "helm.sh/helm/v3/pkg/engine"
-
-	yaml "github.com/goccy/go-yaml"
 )
 
 const LOG_TEST_JOB = "test-job"
@@ -89,28 +86,6 @@ func parseRenderError(errorMessage string) (string, map[string]string) {
 }
 
 func parseYamlFile(rendered string) ([]common.K8sManifest, error) {
-	var yamls []common.K8sManifest
-	rs := bytes.NewReader([]byte(rendered))
-	originalData, err := io.ReadAll(rs)
-	if err != nil {
-		fmt.Println("failed to read config")
-	}
-
-	var content interface{}
-	if err := yaml.Unmarshal(originalData, &content); err != nil {
-		fmt.Println("initial YAML parse error")
-	}
-
-	fmt.Println("Content", content)
-
-	if err := yaml.Unmarshal([]byte(rendered), yamls); err != nil {
-		fmt.Println("Error parsing yaml", err)
-		return nil, err
-	}
-	for _, yml := range yamls {
-		fmt.Println("Yaml", yml)
-	}
-
 	// Replace --- with ---\n to ensure yaml rendering is parsed correctly/
 	rendered = splitterPattern.ReplaceAllString(rendered, "\n---\n")
 	decoder := common.YamlNewDecoder(strings.NewReader(rendered))
